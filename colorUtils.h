aa03f34ca733fa094133c49e46f5dddc78195549
shehzad2004 <fk166494@gmail.com>
2023-12-21 23:26:53 +0500

    origin2

--- colorUtils.h
+++ colorUtils.h
@@ -1,4 +1,3 @@
-
 #ifndef MODE_H
 #define MODE_H
 
@@ -13,19 +12,3 @@ typedef enum {
 /**
  * Returns the maximum value among the three given values
  */
-int max(int a, int b, int c);
-
-/**
- * Returns the minimum value among the three given values
- */
-int min(int a, int b, int c);
-
-/**
- * TODO: add documentation here
- */
-int toGrayScale(int *r, int *g, int *b, Mode mode);
-
-/**
- * TODO: add documentation here
- */
-int toSepia(int *r, int *g, int *b);
